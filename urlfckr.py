9ec018b2f820b28315d1bf3858a545a01b1dfd36
Esteban Zarate <estebanluiszarate1985@gmail.com>
2025-04-29 15:50:11 -0300

    update

--- urlfckr.py
+++ urlfckr.py
@@ -1,11 +1,10 @@
 import argparse
-from urllib.parse import urlparse, urljoin
 import requests
 from bs4 import BeautifulSoup as bs
 import tldextract
-import signal
+from urllib.parse import urlparse, urlsplit, urlunsplit
 import sys
-
+import signal
 
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
@@ -13,10 +12,9 @@ BLUE = "\033[0;34m"
 PURPLE = "\033[0;35m"
 END = "\033[0m"
 
-urls_to_visit = set()
-visited_urls = set()
-relative_urls = set()
-subdomains = set()
+subdomains = []
+urls_to_visit = []
+visited_urls = []
 
 ascii_art = """
                                   ..                           ..                   
@@ -39,97 +37,61 @@ ascii_art = """
 
 def signal_handler(sig, frame):
     print(f"\n[{RED}SEE YA{END}]")
-    print_results()
     sys.exit(0)
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description="URLFCKR")
+    parser = argparse.ArgumentParser(prog="urlfckr", description="Extract subdomains from source code of a website", epilog="https://github.com/n0m3l4c000nt35/urlfckr")
     parser.add_argument("-u", "--url", type=str, dest="url", help="Base URL", required=True)
     args = parser.parse_args()
     return args.url
 
 
-def validate_url(url):
-    try:
-        url_parsed = urlparse(url)
-        if url_parsed.scheme not in ["http", "https"]:
-            raise ValueError(
-                f"Scheme '{url_parsed.scheme}' isn't valid. "
-                "URL must begin with 'http' or 'https'"
-            )
-        return True
-    except ValueError as e:
-        print(f"[{RED}ERROR{END}] {str(e)}")
-
-
-def get_html(url):
-    try:
-        response = requests.get(url)
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print(f"[{RED}ERROR{END}] {str(e)}")
-
-
-def get_hrefs(html):
-    try:
-        soup = bs(html, "html.parser")
-        return [a.get("href") for a in soup.find_all("a")]
-    except Exception as e:
-        print(f"[{RED}ERROR{END}] {str(e)}")
-
-
-def get_valid_hrefs(hrefs, url_base):
-    for href in hrefs:
-        if href and not href.startswith("#"):
-            href_parsed = urlparse(href)
-            if href_parsed.scheme in ["http", "https"] and is_subdomain(url_base, href):
-                subdomains.add(urljoin(href_parsed.scheme + "://" + href_parsed.netloc, "/"))
-                if urljoin(url_base, href) not in visited_urls:
-                    urls_to_visit.add(urljoin(url_base, href))
-            elif not href_parsed.scheme:
-                relative_urls.add(urljoin(url_base, href))
-                if urljoin(url_base, href) not in visited_urls:
-                    urls_to_visit.add(urljoin(url_base, href))
-
-
-def is_subdomain(base_url, url):
-    base_info = tldextract.extract(base_url)
-    base_domain = f"{base_info.domain}.{base_info.suffix}"
-    
-    url_info = tldextract.extract(url)
-    url_domain = f"{url_info.domain}.{url_info.suffix}"
-    
-    return url_domain == base_domain and url_info.subdomain != ""
-
-
-def print_results():
-    print()
-    print(f"[{BLUE}RELATIVE URLS{END}][{PURPLE}{len(relative_urls)}{END}]\n{"\n".join(map(str, sorted(relative_urls)))}" + ("\n" if len(relative_urls) > 0 else ""))
-    print(f"[{BLUE}SUBDOMAINS{END}][{PURPLE}{len(subdomains)}{END}]\n{"\n".join(map(str, sorted(subdomains)))}" + ("\n" if len(subdomains) > 0 else ""))
-    print(f"[{BLUE}VISITED URLS{END}][{PURPLE}{len(visited_urls)}{END}]\n{"\n".join(map(str, sorted(visited_urls)))}" + ("\n" if len(visited_urls) > 0 else ""))
-
-
 def main():
-    
     signal.signal(signal.SIGINT, signal_handler)
+    url = get_args()
+
+    if not urlparse(url).scheme:
+        print("No scheme provided. Please include 'http://' or 'https://' in the URL.")
+        sys.exit(1)
     
-    url_base = get_args()
+    splited_href = urlsplit(url)
+    urlready = urlunsplit((splited_href.scheme, splited_href.netloc, "","",""))
+    urls_to_visit.append(urlready)
 
     print(ascii_art)
 
-    if validate_url(url_base):
-        urls_to_visit.add(url_base)
-        while urls_to_visit:
+    while urls_to_visit:
+        try:
             url_to_visit = urls_to_visit.pop()
-            visited_urls.add(url_to_visit)
-            print(f"[{GREEN}ANALIZING{END}] {url_to_visit}")
-            html = get_html(url_to_visit)
-            hrefs = get_hrefs(html)
-            get_valid_hrefs(hrefs, url_to_visit)
+            print(f"[{BLUE}ANALYZING{END}] {url_to_visit}")
+            response = requests.get(url_to_visit, timeout=3)
+        except requests.exceptions.MissingSchema as e:
+            print("No scheme provided. Please include 'http://' or 'https://' in the URL.")
+        except requests.exceptions.Timeout as e:
+            print("Request timed out. Please check the URL or your internet connection.")
 
+        if response.status_code == 200:
+            soup = bs(response.text, "html.parser")
 
-    print_results()
+            for link in soup.find_all('a'):
+                href = link.get("href")
+                splited_href = urlsplit(href)
+                urlready = urlunsplit((splited_href.scheme, splited_href.netloc, "","",""))
+                if href is not None:
+                    href_extracted = tldextract.extract(href)
+                    url_domain = tldextract.extract(url_to_visit).domain
+                    if href_extracted.domain == url_domain and href_extracted.subdomain != "www":
+                        if urlready not in subdomains:
+                            subdomains.append(urlready)
+                            print(f"[{GREEN}SUBDOMAIN FOUND{END}] {urlready}")
+                            if urlready not in visited_urls:
+                                urls_to_visit.append(urlready)
+            visited_urls.append(url_to_visit)
+        else:
+            print(f"[{RED}ERROR{END}] {response.status_code} - {url_to_visit}")
+    else:
+        print(f"\n[{PURPLE}DONE{END}]")
 
 
 if __name__ == "__main__":
